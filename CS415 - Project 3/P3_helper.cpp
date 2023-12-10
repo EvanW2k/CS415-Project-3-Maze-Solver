f7c2e1bbb5f8c8d26db67dc30514e7f4890d9386
Zeroxa <2991971hz@gmail.com>
2023-12-10 15:55:54 -0800

    Update P3_helper.cpp

--- CS415 - Project 3/P3_helper.cpp
+++ CS415 - Project 3/P3_helper.cpp
@@ -62,8 +62,6 @@ BMP BreadthFirstSearch(BMP I, pixel s, pixel t) {
 	return result;
 }
 
-
-
 int h(pixel u, pixel t) {
 	return abs(u.row - t.row) + abs(u.col - t.col);
 }
@@ -72,48 +70,49 @@ BMP BestFirstSearch(BMP I, pixel s, pixel t) {
 
 	assert(!isObstacle(s, I));
 
-	BMP result = I;
+    BMP result = I;
 
-	priority_queue<pixelPriority> Q;
+    priority_queue<pixelPriority> Q;
 
-	vector<vector<bool>> visited(I.TellHeight(), vector<bool>(I.TellWidth(), false));
-	vector<vector<int>> distance(I.TellHeight(), vector<int>(I.TellWidth(), numeric_limits<int>::max()));
-	vector<vector<pixel>> previous(I.TellHeight(), vector<pixel>(I.TellWidth()));
+    vector<vector<bool>> visited (I.TellHeight(), vector<bool>(I.TellWidth(), false));
+    vector<vector<int>> distance(I.TellHeight(), vector<int>(I.TellWidth(), numeric_limits<int>::max()));
+    vector<vector<pixel>> previous(I.TellHeight(), vector<pixel>(I.TellWidth()));
 
-	visit(s, visited);
-	setDist(s, h(s, t), distance);
+    visit(s, visited);
+    setDist(s, h(s, t), distance);
 
-	Q.push({ s, h(s, t) });
+    Q.push({s, h(s, t)});
 
-	while (!Q.empty() && !(isVisited(t, visited))) {
-		pixel u = Q.top().location;
-		Q.pop();
+    while (!Q.empty() && !(isVisited(t, visited))) {
+        pixel u = Q.top().location;
+        Q.pop();
 
-		if (u != s) makeGreen(u, result);
+        //if (u != s) makeGreen(u, result);
 
-		vector<pixel> neighbors = getNeighbors(u, I);
-		for (int i = 0; i < neighbors.size(); ++i) {
-			if (!isVisited(neighbors[i], visited)) {
-				visit(neighbors[i], visited);
-				int newDist = getDist(u, distance) + 1;
-				setDist(neighbors[i], newDist, distance);
-				setPrev(neighbors[i], u, previous);
-				Q.push({ neighbors[i], newDist + h(neighbors[i], t) });
-			}
-		}
-	}
+        vector<pixel> neighbors = getNeighbors(u, I);
+        for (int i = 0; i < neighbors.size(); ++i) {
+            if (!isVisited(neighbors[i], visited)) {
+                visit(neighbors[i], visited);
+				makeGreen(neighbors[i], result);
+                int newDist = getDist(u, distance) + 1;
+                setDist(neighbors[i], newDist, distance);
+                setPrev(neighbors[i], u, previous);
+                Q.push({neighbors[i], newDist + h(neighbors[i], t)});
+            }
+        }
+    }
 
-	pixel v = t;
-	while (v != s) {
-		makeRed(v, result);
-		v = getPrev(v, previous);
-	}
+    pixel v = t;
+    while (v != s) {
+        makeRed(v, result);
+        v = getPrev(v, previous);
+    }
 
-	// display starter and target pixels as blue
-	makeBlue(s, result);
-	makeBlue(t, result);
+    // display starter and target pixels as blue
+    makeBlue(s, result);
+    makeBlue(t, result);
 
-	return result;
+    return result;
 }
 
 bool isObstacle(pixel p, BMP image) {
